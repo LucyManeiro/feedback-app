18473604156d7bcbeb11d2d9283fa27492a1c7ae
Lucy Maneiro <lucybarnett.ot@gmail.com>
2022-01-06 12:44:35 -0600

    Update feedback functionality added

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,21 +1,22 @@
 import Card from "./shared/Card"
 import PropTypes from "prop-types"
-import {FaTimes} from "react-icons/fa"
+import {FaTimes, FaEdit} from "react-icons/fa"
 import {useContext} from "react"
 import FeedbackContext from "../context/FeedbackContext"
 
 
 function FeedbackItem({item}) {
-    const {deleteFeedback}= useContext(FeedbackContext)
+    const {deleteFeedback, editFeedback}= useContext(FeedbackContext)
     const {rating, text} = item
-    
-    console.log(arguments)
     
     return (
         <Card>
             <div className="num-display">{rating}</div>
             <button onClick={()=> deleteFeedback(item.id)} className="close">
                 <FaTimes color="purple"/>
+            </button>
+            <button onClick={()=> editFeedback(item)} className="edit">
+                <FaEdit color="purple"/>
             </button>
             <div className="text-display">
                 {text}
