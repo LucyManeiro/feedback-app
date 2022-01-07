3869df3a273f839571b32789ede95be2b8b6aced
Lucy Maneiro <lucybarnett.ot@gmail.com>
2022-01-07 12:04:04 -0600

    mock backend functioning, spinner component created

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,20 +1,23 @@
 import FeedbackItem from "./FeedbackItem"
 import {useContext} from "react"
 import FeedbackContext from "../context/FeedbackContext"
+import Spinner from "./shared/Spinner"
 
 function FeedbackList() {
-    const {feedback} = useContext(FeedbackContext)
-    if(!feedback || feedback.length===0){
+    const {feedback, isLoading} = useContext(FeedbackContext)
+
+    if(!isLoading && (!feedback || feedback.length===0)){
         return <p>No Feedback Yet</p>
     }
-    
-     return (
-         <div className="feedback-list">
+     
+    return isLoading ? <Spinner/> : (
+    <div className="feedback-list">
              {feedback.map((item)=> (
                  <FeedbackItem key={item.id} item={item}/>
              ))}
          </div>
-     )
+    )
+     
 }
 
 
